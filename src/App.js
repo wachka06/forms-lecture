2cdd619e46dc8bde55ed9b527a6a74037cd20a82
= <=>
2018-09-19 10:30:42 -0400

    input logger finish

--- src/App.js
+++ src/App.js
@@ -4,25 +4,26 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      lightOn: false
+      loggerText: 'bye'
     }
   }
 
-  handleClick = () => {
-    this.setState((prevState) => {
-      return { lightOn: !prevState.lightOn }
-    }, () => {
-      console.log(this.state)
+  handleInput = (event) => {
+    this.setState({
+      loggerText: event.target.value
     })
   }
 
   render() {
     return (
-      <div className="App">
-        <p>{this.state.lightOn ? 'on' : 'off'}</p>
-        <button onClick={this.handleClick}>Switch</button>
+      <div>
+        <input
+          value={this.state.loggerText}
+          onChange={this.handleInput} 
+          type="text" />
+        <p>{this.state.loggerText}</p>
       </div>
-    );
+    )
   }
 }
 
